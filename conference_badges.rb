f26da33f30f6e6f39ad44810d08e36627cd6f44e
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-03-28 13:06:33 -0400

    commit tree dfd95899735f1c552ff8fb80c73fc1cd7842a765

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers=[])
+    speaker_list = speakers.collect {|speaker| "Hello, my name is #{speaker}."}
+end
+
+def assign_rooms(speakers=[])
+    number = 0
+    rooms = speakers.collect {|speaker| "Hello, #{speaker}! You'll be assigned to room #{number += 1}!"}
+end
+
+def printer(speakers=[])
+    batch_badge_creator(speakers).each {|speaker| puts  speaker}
+    assign_rooms(speakers).each {|speaker|puts  speaker}
+    
+end
